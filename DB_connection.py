70e28d94d68c46d7b70554c46c9afb95e0caff70
AndreyKhodulapov <andrey.corsar1993@gmail.com>
2025-08-31 23:53:15 +0300

    feat: create settings class

--- DB_connection.py
+++ DB_connection.py
@@ -12,26 +12,56 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+
+class Settings:
+    def __init__(self, user: str, 
+                  password: str, 
+                  dbname: str, 
+                  host: str, 
+                  port: int) -> None:
+        if not self.validator(user, password, dbname, host, port):
+            raise ValueError("Invalid settings arguments")
+        self.user = user
+        self.password = password
+        self.dbname = dbname
+        self.host = host
+        self.port = port
+
+    def asdict(self) -> dict[str, str | int]:
+        result = {
+            "user": self.user,
+            "password": self.password,
+            "database": self.dbname,
+            "host": self.host,
+            "port": self.port
+        }
+        return result
+    
+    def validator(self, user: str, 
+                  password: str, 
+                  dbname: str, 
+                  host: str, 
+                  port: int
+            ) -> bool:
+        boolean_attrs = [isinstance(attr, str) for attr in (user, password, dbname, host)]
+        if all(boolean_attrs) and isinstance(port, int):
+            return True
+        return False
+         
+
+
 class DB_connection(metaclass=Singleton):
-    def __init__(self):
+    def __init__(self, settings):
         self.pool: Pool = None
+        self.settings: Settings = settings
         self._is_connected = False
 
-    async def connect(self, **kwargs):
+    async def connect(self):
         """Подключение к базе данных с возможностью кастомизации параметров"""
         if self._is_connected:
             return
-            
-        connection_params = {
-            "user": "postgres",
-            "password": "postgres",
-            "database": "postgres",
-            "host": "localhost",
-            "port": 5432,
-            **kwargs
-        }
         
-        self.pool = await asyncpg.create_pool(**connection_params)
+        self.pool = await asyncpg.create_pool(**self.settings.asdict())
         self._is_connected = True
         logger.info("Database connection established")
 
